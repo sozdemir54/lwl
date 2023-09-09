a0a30ee6df9244eed4e3c61310cfee2566ffcddd
sozdemir54 <97782806+sozdemir54@users.noreply.github.com>
2023-09-09 23:26:25 +0300

    Update dss_new9.py

--- dss_new9.py
+++ dss_new9.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
